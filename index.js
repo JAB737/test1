013397cf55c7e9cc044133bb5053b53a7aecee21
JAB <jorgeacostabello@hotmail.com>
2019-09-29 22:20:39 -0400

    Cree el Procfile.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const http = require('http');
+const connect = require('connect');
+
+function comm(req, res){
+    res.write("Chao");
+    res.end();
+}
+
+http.createServer(comm).listen(2000);
+console.log("Server running on port:2000");
+
